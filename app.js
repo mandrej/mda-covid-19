e81d1900abd83a45cf86152f655ae2990cced819
mandrejevic <mandrejevic@groundlink.com>
2020-04-13 20:53:46 +0200

    SWITCH TO NODEJS RUNTIME

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const path = require(`path`);
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
+
+module.exports = app;
