c57dd05c222a2029a6999ff52c7cb2707baf61e7
mandrejevic <mandrejevic@groundlink.com>
2020-04-13 14:26:33 +0200

    bundle success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='dist', static_folder='dist/static')
+app = Flask(__name__, template_folder='dist',
+            static_url_path='', static_folder='dist')
 
 
 @app.route('/')
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './src/index.js'
     },
+    plugins: [
+        new CopyPlugin([
+            {
+                from: './src/styles.css',
+                to: './styles.css',
+            }
+        ]),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+    ],
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist/static'),
+        filename: 'main.[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
