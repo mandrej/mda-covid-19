5db8d9f54555bc66a43a1db8eb44172aa88a4deb
mandrejevic <mandrejevic@groundlink.com>
2020-04-16 19:52:18 +0200

    single quotes

--- src/chart.js
+++ src/chart.js
@@ -1,30 +1,30 @@
-import * as d3 from "d3";
+import * as d3 from 'd3';
 
 const margin = { top: 20, right: 50, bottom: 50, left: 50 },
     W = 800, H = 500,
     width = W - margin.left - margin.right,
     height = H - margin.top - margin.bottom;
 
-const dateFormat = d3.timeFormat("%d.%m.%Y");
-const numberFormat = d3.format(",.2f")
-const parseDate = d3.timeParse("%Y-%m-%d");
+const dateFormat = d3.timeFormat('%d.%m.%Y');
+const numberFormat = d3.format(',.2f')
+const parseDate = d3.timeParse('%Y-%m-%d');
 
 const locations = {
-    "Serbia": 6.804596,
-    "Slovenia": 2.078932,
-    "Croatia": 4.105268,
-    "Romania": 19.237682,
-    "Spain": 46.754783,
-    "Italy": 60.461828,
-    "Germany": 83.783945,
-    "United States": 331.002647,
-    "China": 1439.323774
+    'Serbia': 6.804596,
+    'Slovenia': 2.078932,
+    'Croatia': 4.105268,
+    'Romania': 19.237682,
+    'Spain': 46.754783,
+    'Italy': 60.461828,
+    'Germany': 83.783945,
+    'United States': 331.002647,
+    'China': 1439.323774
 }
 let count = 0;
 export default function (countries, raw, value, perMillion, axesFormat, title) {
     count++;
     const data = raw.filter(d => {
-        return countries.indexOf(d.location) >= 0 && +d["new_cases"] > 0
+        return countries.indexOf(d.location) >= 0 && +d['new_cases'] > 0
     }).map(d => {
         // patching
         if (d.date === '2020-04-14' && d.location === 'Serbia') {
@@ -48,7 +48,7 @@ export default function (countries, raw, value, perMillion, axesFormat, title) {
         return {
             location: d.location,
             date: parseDate(d.date),
-            total: (perMillion) ? +d[value] / locations[d.location] : +d[value] / +d["total_cases"]
+            total: (perMillion) ? +d[value] / locations[d.location] : +d[value] / +d['total_cases']
         }
     });
     const byLocation = countries.map(loc => {
@@ -87,7 +87,7 @@ export default function (countries, raw, value, perMillion, axesFormat, title) {
         .domain(byLocation.map(c => { return c.location; }));
 
     const latest = d3.max(data, d => { return d.date });
-    document.getElementById("latest").innerHTML = dateFormat(latest);
+    document.getElementById('latest').innerHTML = dateFormat(latest);
 
     // line generator
     const line = d3.line()
@@ -105,109 +105,109 @@ export default function (countries, raw, value, perMillion, axesFormat, title) {
             .ticks(10)
     };
 
-    const svg = d3.select("body").append("svg")
-        // .attr("width", W)
-        // .attr("height", H)
-        .attr("id", "g" + count)
-        .attr("preserveAspectRatio", "xMinYMin meet")
-        .attr("viewBox", "0 0 " + W + " " + H)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    const svg = d3.select('body').append('svg')
+        // .attr('width', W)
+        // .attr('height', H)
+        .attr('id', 'g' + count)
+        .attr('preserveAspectRatio', 'xMinYMin meet')
+        .attr('viewBox', '0 0 ' + W + ' ' + H)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
     // add axis 
-    svg.append("g")
-        .attr("class", "axes")
-        .attr("transform", "translate(0," + height + ")")
+    svg.append('g')
+        .attr('class', 'axes')
+        .attr('transform', 'translate(0,' + height + ')')
         .call(d3.axisBottom(xScale)
-            .tickFormat(d3.timeFormat("%d.%m")));
-    svg.append("g")
-        .attr("class", "axes")
+            .tickFormat(d3.timeFormat('%d.%m')));
+    svg.append('g')
+        .attr('class', 'axes')
         .call(d3.axisLeft(yScale)
             .ticks(5)
             .tickFormat(axesFormat));
-    svg.append("g")
-        .attr("class", "axes")
-        .attr("transform", "translate(" + width + ", 0)")
+    svg.append('g')
+        .attr('class', 'axes')
+        .attr('transform', 'translate(' + width + ', 0)')
         .call(d3.axisRight(yScale)
             .ticks(5)
             .tickFormat(axesFormat));
 
     // add gridlines
-    svg.append("g")
-        .attr("class", "grid")
-        .attr("transform", "translate(0," + height + ")")
+    svg.append('g')
+        .attr('class', 'grid')
+        .attr('transform', 'translate(0,' + height + ')')
         .call(make_x_gridlines()
             .tickSize(-height)
-            .tickFormat(""));
-    svg.append("g")
-        .attr("class", "grid")
+            .tickFormat(''));
+    svg.append('g')
+        .attr('class', 'grid')
         .call(make_y_gridlines()
             .tickSize(-width)
-            .tickFormat(""));
+            .tickFormat(''));
 
     // add x-label
-    svg.append("text")
-        .attr("class", "label")
-        .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 20) + ")")
-        .style("text-anchor", "middle")
-        .text("date");
-    svg.append("text")
-        .attr("class", "label")
-        .attr("transform", "translate(" + width + " ," + (height + margin.top + 20) + ")")
-        .style("text-anchor", "end")
-        .text("© https://mda-covid-19.appspot.com/");
+    svg.append('text')
+        .attr('class', 'label')
+        .attr('transform', 'translate(' + (width / 2) + ' ,' + (height + margin.top + 20) + ')')
+        .style('text-anchor', 'middle')
+        .text('date');
+    svg.append('text')
+        .attr('class', 'label')
+        .attr('transform', 'translate(' + width + ' ,' + (height + margin.top + 20) + ')')
+        .style('text-anchor', 'end')
+        .text('© https://mda-covid-19.appspot.com/');
 
     // add legend
-    const legend = svg.append("g")
-        .attr("class", "legend")
-        .attr("transform", "translate(" + (margin.left - 30) + "," + (margin.top + 20) + ")");
-    legend.append("text")
-        .style("font-size", "20px")
+    const legend = svg.append('g')
+        .attr('class', 'legend')
+        .attr('transform', 'translate(' + (margin.left - 30) + ',' + (margin.top + 20) + ')');
+    legend.append('text')
+        .style('font-size', '20px')
         .text(title);
-    legend.selectAll("circle")
+    legend.selectAll('circle')
         .data(countries)
         .enter()
-        .append("circle")
-        .attr("cx", 10)
-        .attr("cy", (d, i) => { return 20 + i * 20 }) // 100 is where the first dot appears. 25 is the distance between dots
-        .attr("r", 7)
-        .style("fill", d => { return zScale(d) });
-    legend.selectAll(".country")
+        .append('circle')
+        .attr('cx', 10)
+        .attr('cy', (d, i) => { return 20 + i * 20 }) // 100 is where the first dot appears. 25 is the distance between dots
+        .attr('r', 7)
+        .style('fill', d => { return zScale(d) });
+    legend.selectAll('.country')
         .data(countries)
         .enter()
-        .append("text")
-        .attr("class", "country")
-        .attr("x", 20)
-        .attr("y", (d, i) => { return 21 + i * 20 }) // 100 is where the first dot appears. 25 is the distance between dots
-        .style("fill", d => { return zScale(d[0]) })
-        .text(d => { return d + " (" + numberFormat(locations[d]) + " mil.)" })
-        .style("font-size", "14px")
-        .style("alignment-baseline", "middle");
+        .append('text')
+        .attr('class', 'country')
+        .attr('x', 20)
+        .attr('y', (d, i) => { return 21 + i * 20 }) // 100 is where the first dot appears. 25 is the distance between dots
+        .style('fill', d => { return zScale(d[0]) })
+        .text(d => { return d + ' (' + numberFormat(locations[d]) + ' mil.)' })
+        .style('font-size', '14px')
+        .style('alignment-baseline', 'middle');
 
-    svg.selectAll(".dot")
+    svg.selectAll('.dot')
         .data(data)
         .enter()
-        .append("circle")
-        .attr("class", "dot")
-        .attr("cx", d => { return xScale(d.date) })
-        .attr("cy", d => { return yScale(d.total) })
-        .attr("r", 4)
-        .style("fill", d => { return zScale(d.location) })
-        .append("title")
+        .append('circle')
+        .attr('class', 'dot')
+        .attr('cx', d => { return xScale(d.date) })
+        .attr('cy', d => { return yScale(d.total) })
+        .attr('r', 4)
+        .style('fill', d => { return zScale(d.location) })
+        .append('title')
         .text(d => {
             return (perMillion) ?
-                d.location + "\n" + dateFormat(d.date) + "\n" + numberFormat(d.total) :
-                d.location + "\n" + dateFormat(d.date) + "\n" + axesFormat(d.total)
+                d.location + '\n' + dateFormat(d.date) + '\n' + numberFormat(d.total) :
+                d.location + '\n' + dateFormat(d.date) + '\n' + axesFormat(d.total)
         });
 
     // add lines byLocation
-    const loc = svg.selectAll(".location")
+    const loc = svg.selectAll('.location')
         .data(byLocation)
-        .enter().append("g")
-        .attr("class", "location");
+        .enter().append('g')
+        .attr('class', 'location');
 
-    loc.append("path")
-        .attr("class", "line")
-        .attr("d", d => { return line(d.values); })
-        .style("stroke", d => { return zScale(d.location) });
+    loc.append('path')
+        .attr('class', 'line')
+        .attr('d', d => { return line(d.values); })
+        .style('stroke', d => { return zScale(d.location) });
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import * as d3 from "d3";
-import chart from "./chart";
+import * as d3 from 'd3';
+import chart from './chart';
 import d3ToPng from 'd3-svg-to-png';
 
 const title = {
-    1: "Total cases / per million",
-    2: "Daily cases / per million",
-    3: "Total deaths / total cases"
+    1: 'Total cases / per million',
+    2: 'Daily cases / per million',
+    3: 'Total deaths / total cases'
 }
 
 const choose = function () {
-    const nodes = document.querySelectorAll("input[name='country']:checked");
+    const nodes = document.querySelectorAll('input[name="country"]:checked');
     const selected = Array.from(nodes).map(sel => {
         return sel.value
     })
@@ -18,7 +18,7 @@ const choose = function () {
 
 export function save (num) {
     // https://www.npmjs.com/package/d3-svg-to-png
-    d3ToPng("#g" + num, title[num], {
+    d3ToPng('#g' + num, title[num], {
         scale: 1,
         download: true
     }).then(file => {
@@ -29,12 +29,12 @@ export function save (num) {
 export function draw () {
     const countries = choose();
 
-    d3.csv("https://covid.ourworldindata.org/data/ecdc/full_data.csv").then(raw => {
-        d3.selectAll("svg").remove();
+    d3.csv('https://covid.ourworldindata.org/data/ecdc/full_data.csv').then(raw => {
+        d3.selectAll('svg').remove();
         //   (countries, raw, value, perMillion, axesFormat, title)
-        chart(countries, raw, "total_cases", true, d3.format("1,"), title[1]);
-        chart(countries, raw, "new_cases", true, d3.format("1,"), title[2]);
-        chart(countries, raw, "total_deaths", false, d3.format(",.1%"), title[3]);
+        chart(countries, raw, 'total_cases', true, d3.format('1,'), title[1]);
+        chart(countries, raw, 'new_cases', true, d3.format('1,'), title[2]);
+        chart(countries, raw, 'total_deaths', false, d3.format(',.1%'), title[3]);
     })
 }
 
